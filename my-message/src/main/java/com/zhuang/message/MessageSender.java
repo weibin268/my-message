0c9358943ef11170e691a56689d24fb4a528cb11
庄伟斌 <zhuangweibin@qq.com>
2020-12-21 16:44:27 +0800

    aa

---
+++ my-message/src/main/java/com/zhuang/message/MessageSender.java
@@ -0,0 +1,9 @@
+package com.zhuang.message;
+
+import java.util.Map;
+
+public interface MessageSender {
+
+    boolean send(String templateId, Map<String, Object> params);
+
+}
---
+++ my-message/src/main/java/com/zhuang/message/sms/AliYunSmsSender.java
@@ -0,0 +1,39 @@
+package com.zhuang.message.sms;
+
+import com.aliyuncs.CommonRequest;
+import com.aliyuncs.CommonResponse;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.exceptions.ServerException;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.profile.DefaultProfile;
+import com.zhuang.message.MessageSender;
+
+import java.util.Map;
+
+public class AliYunSmsSender implements MessageSender {
+
+    @Override
+    public boolean send(String templateId, Map<String, Object> params) {
+        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "<accessKeyId>", "<accessSecret>");
+        IAcsClient client = new DefaultAcsClient(profile);
+
+        CommonRequest request = new CommonRequest();
+        request.setSysMethod(MethodType.POST);
+        request.setSysDomain("dysmsapi.aliyuncs.com");
+        request.setSysVersion("2017-05-25");
+        request.setSysAction("SendSms");
+        request.putQueryParameter("RegionId", "cn-hangzhou");
+        try {
+            CommonResponse response = client.getCommonResponse(request);
+            System.out.println(response.getData());
+        } catch (ServerException e) {
+            e.printStackTrace();
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+}
