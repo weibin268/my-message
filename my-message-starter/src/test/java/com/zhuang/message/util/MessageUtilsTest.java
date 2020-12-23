6ae614b9f80e0f9698ec1acb8ddb154e6cddd91f
庄伟斌 <zhuangweibin@qq.com>
2020-12-23 17:43:25 +0800

    aa

---
+++ my-message-starter/src/test/java/com/zhuang/message/util/MessageUtilsTest.java
@@ -0,0 +1,15 @@
+package com.zhuang.message.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MessageUtilsTest {
+
+    @Test
+    public void sendSms() {
+
+
+
+    }
+}
