bc16261477c901c33d32b1e8cb97307a09d5b63e
庄伟斌 <zhuangweibin@qq.com>
2020-12-24 09:41:42 +0800

    aa

--- my-message/src/main/java/com/zhuang/message/db/mysql.create.sql
+++ my-message/src/main/java/com/zhuang/message/db/mysql.create.sql
@@ -2,7 +2,7 @@ create table sys_message_log
 (
 	id varchar(36) not null,
 	type varchar(10) null comment '消息类型',
-	template_id varchar(50) null comment '模板Id',
+	template_id varchar(36) null comment '模板Id',
 	params varchar(500) null comment '参数',
 	to_users varchar (500) null comment '接收用户',
 	content varchar(200) null comment '消息内容',
@@ -14,5 +14,20 @@ create table sys_message_log
     modify_by   VARCHAR(50)
 	primary key (id)
 )
-comment '消息发送日志';
+comment '消息日志';
+
+create table sys_message_template
+(
+	id varchar(36) not null,
+	name varchar(10) null comment '模板名字',
+    content varchar(200) null comment '模板内容',
+	params varchar(500) null comment '参数',
+	status int null comment '状态：0=启用；1=禁用；',
+    create_time DATETIME,
+    modify_time DATETIME,
+    create_by   VARCHAR(50),
+    modify_by   VARCHAR(50)
+	primary key (id)
+)
+comment '消息模板';
 
