628359276f134f8af9e7458a28818cb22bd9d2ef
庄伟斌 <zhuangweibin@qq.com>
2024-04-19 11:41:19 +0800

    增加移动mas云短信

--- my-message-starter/src/main/java/com/zhuang/message/util/MessageUtils.java
+++ my-message-starter/src/main/java/com/zhuang/message/util/MessageUtils.java
@@ -1,17 +1,22 @@
 package com.zhuang.message.util;
 
 import com.zhuang.message.MessageSender;
+import com.zhuang.message.config.MyMessageProperties;
 import com.zhuang.message.constant.MsgParams;
 import com.zhuang.message.enums.MessageType;
 import com.zhuang.message.enums.MsgType;
 import com.zhuang.message.model.SendResult;
+import com.zhuang.message.sms.AliYunSmsSender;
+import com.zhuang.message.sms.MasSmsSender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Component
 public class MessageUtils {
@@ -19,6 +24,8 @@ public class MessageUtils {
     private static MessageUtils _this;
     @Autowired
     private List<MessageSender> messageSenderList;
+    @Autowired
+    private MyMessageProperties myMessageProperties;
 
     @PostConstruct
     private void init() {
@@ -26,12 +33,23 @@ public class MessageUtils {
     }
 
     public static SendResult send(MessageType messageType, String templateId, Map<String, Object> params, String toUsers) {
-        Optional<MessageSender> optionalMessageSender = _this.messageSenderList.stream().filter(c -> c.getMessageType() == messageType).findFirst();
-        if (optionalMessageSender.isPresent()) {
-            return optionalMessageSender.get().send(templateId, params, toUsers);
-        } else {
+        List<MessageSender> senderList = _this.messageSenderList.stream().filter(c -> c.getMessageType() == messageType).collect(Collectors.toList());
+        if (CollectionUtils.isEmpty(senderList)) {
             throw new RuntimeException("can not find the message sender!");
         }
+        MessageSender messageSender;
+        if (senderList.size() == 1) {
+            messageSender = senderList.get(0);
+        } else if (senderList.stream().allMatch(c -> c.getMessageType().equals(MessageType.SMS))) {
+            if ("sms4Mas".equals(_this.myMessageProperties.getDefaultSmsType()) || "*".equals(templateId)) {
+                messageSender = senderList.stream().filter(c -> c instanceof MasSmsSender).findFirst().orElse(null);
+            } else {
+                messageSender = senderList.stream().filter(c -> c instanceof AliYunSmsSender).findFirst().orElse(null);
+            }
+        } else {
+            throw new RuntimeException("found more one the message sender!");
+        }
+        return messageSender.send(templateId, params, toUsers);
     }
 
     public static SendResult sendSms(String templateId, Map<String, Object> params, String toUsers) {
